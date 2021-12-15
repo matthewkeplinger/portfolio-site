bdce33fba8658a460ffab194f4f325e5bab7cd6c
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-12-15 09:59:03 -0500

    Finished Intro Section; added ityped from NPM for CSS animations

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,38 @@
 import "./Intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
 function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            backDelay:1500,
+            backSpeed:60,
+            strings:["Developer", "Designer", "Problem Solver"],
+        });
+    },[])
+
+
     return (
         <div className = "intro" id = "intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src = "./assets/author.png" alt = ''/>
+                </div>
+            </div>
+            <div className = "right">
+                <div className = "wrapper">
+                    <h2>Hello! I'm</h2>
+                    <h1>Matt Keplinger</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio" alt = "">
+                    <img src = "assets/down.png" alt = ""/>
+                </a>
+            </div>
         </div>
     )
 }
